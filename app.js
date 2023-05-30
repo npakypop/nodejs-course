9d6e39bc3d225dc799360352c9dccb55d5050253
AntonKh <leaterood@gmail.com>
2023-05-30 12:21:45 +0300

    routing controllers

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatsLogger));
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); //! из-за того что тело запроса может быть отправлено в разных форматах и для того что бы экспресс понял какой формат был отправлен и преобразовать его обьект используется такая мидлвара(она проверяет каждый запрос и есть ли в нем тело, если есть смотрит какого типа оно по заголовку content-type: )
 
 app.use("/api/contacts", contactsRouter); //! если появился какой либо запрос с таким маршрутом то его обработчик лежит в этом contactsRouter
 
@@ -19,7 +19,8 @@ app.use((req, res) => {
 }); //! что делать если пришел запрос на адрес которого нет
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message });
-});
+  const { status = 500, message = "server error" } = err; //! здесь по умолчанию в случае ошибки будет ошибка сервера 500
+  res.status(status).json({ message });
+}); //! эту функцию будет искать next(error) из контроллера для обработки ошибок. Где бы в коде не написали next(error) то будет найдена именно эта функция
 
 module.exports = app;
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,33 +1,97 @@
 const express = require("express");
-
+const Joi = require("joi"); //! библиотека для проверки тела запроса
 const contacts = require("../../models/contacts"); //! от сюда достаем функции запросов и вставляем их в обработчики маршутов
 
 const router = express.Router();
 //! тут у маршрутов ниже не указан полный путь /api/contacts так как в app.js уже указали это в роутинге и если запрос совпал с тем что укказали то тогда все остальное ищет здесь
 
+const { HttpError } = require("../../helpers");
+
+const addSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+});
+
 router.get("/", async (req, res, next) => {
-  const result = await contacts.listContacts();
-  res.json(result);
+  try {
+    const result = await contacts.listContacts();
+    res.json(result);
+  } catch (error) {
+    res.status(500).json({ message: "Server error ebobo" }); //! если произошла ошибка с базой данный на сервере то тогда будет отправлена ошибка на фронтенд
+  }
 });
 
 router.get("/:contactId", async (req, res, next) => {
-  const result = await contacts.getContactById();
-  res.json(result);
+  try {
+    const { contactId } = req.params; //! параметры запроса
+    const result = await contacts.getContactById(contactId);
+    if (!result) {
+      throw HttpError(404, "Not found уищищ"); //! эта функция которая выполоняет код в 3 строках ниже
+      // const error = new Error("Not found");
+      // error.status = 404;
+      // throw error; //! прокидываем ошибку в catch так как обрабатывать ошибку лучше в одном месте.
+      // return res.status(404).json({
+      //   message: "Not found",
+      // });
+    }
+    res.json(result);
+  } catch (error) {
+    next(error); //! если передать в некст ошибку то тогда он будет искать именно обработчик ошибок. Обработчик ошибок это функция с 4 параметрами(err, req, res, next) и именно такую функцию будет искать некст) эта функция лежит в файле апп и это единственная функция с 4 параметрами.
+    // const { status = 500, message = "Server error" } = error;
+    // res.status(status).json({ message });
+  }
 });
 
 router.post("/", async (req, res, next) => {
-  const result = await contacts.addContact();
-  res.json(result);
-});
-
-router.delete("/:contactId", async (req, res, next) => {
-  const result = await contacts.removeContact();
-  res.json(result);
+  try {
+    const body = req.body;
+    const { error } = addSchema.validate(body); //! валидация тела запроса
+    if (error) {
+      //! перед тем как добавить контакт проверяем есть ли ошибка после валидации
+      throw HttpError(400, error.message);
+    }
+    const result = await contacts.addContact(body);
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.put("/:contactId", async (req, res, next) => {
-  const result = await contacts.updateContact();
-  res.json(result);
+  try {
+    const { error } = addSchema.validate(req.body); //! валидация тела запроса
+    if (error) {
+      //! перед тем как обновить контакт проверяем есть ли ошибка после валидации
+      throw HttpError(400, error.message);
+    }
+    const result = await contacts.updateContact(req.params.contactId, req.body);
+    if (!result) {
+      throw HttpError(404, "Not found");
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+}); //! поскольку этот запрос обновляет абсолютно весь объект, от в тело запроса надо передавать все поля и те которые я хочу обновить и те которые остануться без обновления
+
+router.delete("/:contactId", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const result = await contacts.removeContact(contactId);
+    if (!result) {
+      throw HttpError(404, "Not found");
+    }
+    // !  что возвращать после удаления зависит от ситуации и поставленой задачи, ниже два варианта либо сообщение об удасном удалении  либо обьект который удалили
+    // ?   ================== 1 вариант
+    res.json({
+      message: "Delete success",
+    });
+    // ?   ================== 2 вариант
+    // res.json(result);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
